cd0a69e5c62c59bd24ca7bc0a83a4b83ec526a34
THE HB <82237720+hbbots@users.noreply.github.com>
2025-07-23 11:50:44 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,18 +9,18 @@ import { getFirestore } from 'firebase/firestore';
 
 // ✅ Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBLh8l3aKhLwceU--JGM8Iwm_wIyW_gUm4",
-  authDomain: "planfitapp-60ee0.firebaseapp.com",
-  projectId: "planfitapp-60ee0",
-  storageBucket: "planfitapp-60ee0.appspot.com",
-  messagingSenderId: "864713744333",
-  appId: "1:864713744333:web:1334861851298c37daee84",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
-// ✅ Initialize app (handles hot reloads)
+
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-// ✅ Initialize auth with AsyncStorage for persistence
+
 let auth;
 try {
   auth = initializeAuth(app, {
@@ -30,7 +30,7 @@ try {
   auth = getAuth(app); // fallback
 }
 
-// ✅ Initialize Firestore
+
 const db = getFirestore(app);
 
 export { app, auth, db };
