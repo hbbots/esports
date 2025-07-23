b06ae3d54ca4dfe6221d992c7fdefb051a2b4923
THE HB <82237720+hbbots@users.noreply.github.com>
2025-07-23 11:49:02 +0530

    Create _layout.tsx

---
+++ app/legal/_layout.tsx
@@ -0,0 +1,9 @@
+// app/legal/_layout.tsx
+
+import { Stack } from 'expo-router';
+
+export default function LegalLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }} />
+  );
+}
